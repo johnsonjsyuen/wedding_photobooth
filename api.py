269278249e55ef1fd677917649e5fea6e4a5b57c
johnsonyuenSEEK <jyuen@seek.com.au>
2019-11-23 21:31:08 +1100

    Styled with bootstrap

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from starlette.staticfiles import StaticFiles
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
-from starlette.routing import Route
+from starlette.routing import Route, Mount
 import uvicorn
 
 
@@ -21,16 +21,23 @@ async def homepage(request):
         form = await request.form()
         print(form)
         print(form.getlist('file'))
+        successful_uploads = 0
         for uploadFile in form.getlist('file'):
             print(uploadFile.filename)
             print(uploadFile.content_type)
             with open('uploads/'+uploadFile.filename,'wb') as f:
                 f.write(await uploadFile.read())
+                successful_uploads += 1
 
 
-        return JSONResponse("wrote body to file")
 
-routes = [Route('/',endpoint=homepage,methods=['GET','POST'])]
+        template = "index.html"
+        context = {"request": request, "files_uploaded":successful_uploads}
+        return templates.TemplateResponse(template, context)
+
+
+routes = [Route('/',endpoint=homepage,methods=['GET','POST']),
+          Mount('/static', StaticFiles(directory='statics'), name='static')]
 app = Starlette(debug=True, routes=routes)
 
 if __name__ == "__main__":
